78c41212375b5e4ab37733fd37b4d475e239c80f
matheus <matha.nogueira@gmail.com>
2014-10-10 23:51:43 -0300

    b

--- LSCuda/nextperm.cu
+++ LSCuda/nextperm.cu
@@ -190,7 +190,7 @@ int main(int argc, char *argv[]){
     //Número de elementos em cada conjunto. length (rotação) * 2 (inversão)    
 	int tamGroup = 2*length;
 
-	/*
+
 	//Cada loop gera um conjunto de sequências. Elementos de S. Cada elemento possui um conjunto de R sequencias.
 	while(counter){
 		
@@ -203,7 +203,7 @@ int main(int argc, char *argv[]){
 		if(numSeqReadyAnt != 0){
 			//cudaThreadSynchronize();
 			//Envia os resultados obtidos para o host
-			cudaMemcpy(h_lMin_s, d_lMin_s, sizeof(unsigned int)*numSeqReady, cudaMemcpyDeviceToHost);
+			//cudaMemcpy(h_lMin_s, d_lMin_s, sizeof(unsigned int)*numSeqReady, cudaMemcpyDeviceToHost);
 
 			//calcLMaxS(&lMax_S, h_lMin_s, numSeqReadyAnt, tamGroup);
 		}
@@ -211,15 +211,15 @@ int main(int argc, char *argv[]){
 		//Caso não tenha como inserir mais un conjunto inteiro no número de threads, então executa:
 		if((numSeqReady+tamGroup) >= NUM_THREADS){
 			//printf("	Entrou 1\n");
-			cudaMemcpy(d_threadSequences, h_threadSequences, sizeof(int)*numSeqReady*length, cudaMemcpyHostToDevice);
+			//cudaMemcpy(d_threadSequences, h_threadSequences, sizeof(int)*numSeqReady*length, cudaMemcpyHostToDevice);
 			
 			//printf("	Entrou 2 \n");
 			//Cada thread calcula o LIS e o LDS de cada sequência
 			dim3 num_blocks(ceil(((float) numSeqReady)/(float) THREAD_PER_BLOCK));
 			int tam_shared = ((length+1)*(length+1)+2*length)*THREAD_PER_BLOCK*sizeof(int);
 			//printf("	Entrou 3 \n");
-			decideLS<<<THREAD_PER_BLOCK, num_blocks, tam_shared>>>
-					   (d_threadSequences, d_lMin_s, length, numSeqReady);
+			/*decideLS<<<THREAD_PER_BLOCK, num_blocks, tam_shared>>>
+					   (d_threadSequences, d_lMin_s, length, numSeqReady);*/
 
 			//printf("	Entrou 4  \n");
 			numSeqReadyAnt = numSeqReady;
@@ -229,7 +229,7 @@ int main(int argc, char *argv[]){
 		//Cria a próxima sequência na ordem lexicográfica
 		next_permutation(h_sequence+1,length-1);
 		counter--;
-	}*/
+	}
 
 	end = clock();
 
