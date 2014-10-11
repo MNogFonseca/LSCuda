74e1c5e538750c70e3624af884855ab27d490cf8
matheus <matha.nogueira@gmail.com>
2014-10-11 11:12:58 -0300

    b

--- LSCuda/nextperm.cu
+++ LSCuda/nextperm.cu
@@ -102,10 +102,8 @@ void decideLS(int *vector, unsigned int* lmin, int length, int numThread){
 	int tid = threadIdx.x + blockIdx.x*blockDim.x; 
 	int s_step = (length+1)*(length+1) + 2*length;
 	int s_index = s_step*threadIdx.x; //Indice da shared memory
-	printf("AA%d - %d\n", tid, numThread);
 	if(tid < numThread){
 		int i;
-		printf("BB%d\n", tid);
 		for(i = 0; i < length; i++){
 			s_vet[s_index*threadIdx.x+i] = vector[tid*length+i];
 		}
@@ -216,7 +214,7 @@ int main(int argc, char *argv[]){
 			//Cada thread calcula o LIS e o LDS de cada sequência
 			dim3 num_blocks(ceil(((float) numSeqReady)/(float) THREAD_PER_BLOCK));
 			int tam_shared = ((length+1)*(length+1)+2*length)*THREAD_PER_BLOCK*sizeof(int);
-			
+			printf("%d\n", tam_shared);
 			decideLS<<<THREAD_PER_BLOCK, num_blocks, tam_shared>>>
 					   (d_threadSequences, d_lMin_s, length, numSeqReady);
 			cudaGetLastError();
