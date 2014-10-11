39a15476639c9c8db37313dd86d78d529858b637
matheus <matha.nogueira@gmail.com>
2014-10-10 22:08:26 -0300

    b

--- LSCuda/nextperm.cu
+++ LSCuda/nextperm.cu
@@ -214,7 +214,7 @@ int main(int argc, char *argv[]){
 			cudaMemcpy(d_threadSequences, h_threadSequences, sizeof(int)*numSeqReady*length, cudaMemcpyHostToDevice);
 			//Cada thread calcula o LIS e o LDS de cada sequência
 			dim3 num_blocks(ceil(((float) numSeqReady)/(float) THREAD_PER_BLOCK));
-			printf("%d\n",num_blocks.x);
+			printf("%d\n",numSeqReady);
 			int tam_shared = ((length+1)*(length+1)+2*length)*64*sizeof(int);
 			decideLS<<<THREAD_PER_BLOCK, num_blocks, tam_shared>>>
 					   (d_threadSequences, d_lMin_s, length, numSeqReady);
