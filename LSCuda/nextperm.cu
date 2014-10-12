961a08c54fbc50117d1864eb993eb0d238e6c3a5
matheus <matha.nogueira@gmail.com>
2014-10-12 11:11:03 -0300

    b

--- LSCuda/nextperm.cu
+++ LSCuda/nextperm.cu
@@ -171,7 +171,7 @@ int main(int argc, char *argv[]){
 	unsigned int* h_lMin_R;      
 
 	int length = atoi(argv[1]);
-	int NUM_THREADS = atoi(argv[2)
+	int NUM_THREADS = atoi(argv[2);
 	
 	//Tamanho linear da sequência que vai ser enviada para cada thread.
 	//Vetor consisti em Sua sequência seguida por repetição dos seus primeiros length-1 elementos devido a rotação.
