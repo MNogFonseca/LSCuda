62c1314d8f4a3050eb21f97a524e01f4a29dfe3e
matheus <matha.nogueira@gmail.com>
2014-10-17 13:38:18 -0300

    b

--- LSCuda/nextperm.cu
+++ LSCuda/nextperm.cu
@@ -243,7 +243,7 @@ int main(int argc, char *argv[]){
 
 		if((counterMax - counter)%(counterMax/100) == 0){
 			end = clock();
-			printf("%d%% - Tempo: %f s  - Lmax S %d - Counter: %lu\n",(int)(counterMax - counter)/(counterMax/100), (float)(end-start)/CLOCKS_PER_SEC, lMax_S, counter);
+			printf("%d%% - Tempo: %f s  - Lmax S %d - Counter: %lu\n",(int)((counterMax - counter)/(counterMax/100)), (float)(end-start)/CLOCKS_PER_SEC, lMax_S, counter);
 		}
 	}
 
