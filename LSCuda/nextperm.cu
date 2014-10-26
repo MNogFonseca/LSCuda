be30b47e3caed8b39188dd883b96c6cc758d59cb
matheus <matha.nogueira@gmail.com>
2014-10-26 16:51:19 -0200

    b

--- LSCuda/nextperm.cu
+++ LSCuda/nextperm.cu
@@ -37,6 +37,7 @@ void rotation(char *array, int length){
   array[i] = temp;
 }
 
+//faz a proxima permutação na ordem lexicográfica
 int next_permutation(char *array, size_t length) {
 	size_t i, j;
 	char temp;
@@ -78,27 +79,35 @@ unsigned long fatorial(unsigned long n){
 	return result;
 }
 
-//Min(|LIS(s)|, |LDS(s)|)
+//Calcula o LIS de todo o conjunto R partindo do pivor principal da ordem lexico gráfica
+//Caso encontre um valor que é menor do que o máximo local de S, então ele retorna e não faz os outros calculos.
 __global__
 void decideLS(char *vector, char* d_lMax_S, int length, int numThread){
-	//Step_shared - quantidade de posições utilizada por cada thread
-	//length - quantidade de posições utilizadas pela sequência
-	//step_last - quantidade de posições utilizado pelo vetor Lasto do LSI/LDS
 	extern __shared__ char s_vet[];
 	int tid = threadIdx.x + blockIdx.x*blockDim.x; 	
 	int s_index = length*threadIdx.x; //Indice da shared memory
+
 	if(tid < numThread){
+		//carrega o vetor na shared memory
 		for(int i = 0; i < length; i++){
 			s_vet[s_index+i] = vector[tid*length+i];
 		}
 		
-		char MP[N*(N+1)/2];
-		char last[N];
+		//Esses dois vetores são utilizados no LIS e no LDS, são declarados do lado de fora para
+		//gastar menos memória e não ter necessidade de dar malloc.
+		char MP[N*(N+1)/2]; //Vetor de most promising
+		char last[N]; //Vetor de last de MP
+
+		//Valores com os resultados encontrados no LIS e no LDS
 		char lLIS, lLDS; 
-		char lMin_R = 127;
+
+		char lMin_R = 127; //Variavel que representa o min encontrado no conjunto R
+
 		for(int j = 0; j < 2; j++){ //Inverção
-			for(int i = 0; i < length; i++){
+			for(int i = 0; i < length; i++){ //Rotação
+
 				lLIS = LIS(s_vet + s_index, last, MP, length);
+				//caso seja menor que o minimo do conjunto R, então modificar o valor
 				if(lLIS < lMin_R){
 					lMin_R = lLIS;	
 				}
@@ -109,13 +118,12 @@ void decideLS(char *vector, char* d_lMax_S, int length, int numThread){
 				}
 
 				lLDS = LDS(s_vet + s_index, last, MP, length);
+				//caso seja menor que o minimo do conjunto R, então modificar o valor
 				if(lLDS < lMin_R){				
 					lMin_R = lLDS;
 				}
 
 				//Todo o conjunto pode ser descartado, pois não vai subistituir lMax_S no resultado final
-				
-
 				if(lLDS <= d_lMax_S[tid]){
 					return;
 				}
@@ -130,18 +138,22 @@ void decideLS(char *vector, char* d_lMax_S, int length, int numThread){
 				inversion(s_vet + s_index, length);
 			}
 		}
+		//Caso o resultado final encontrado de R chegue ate o final, então significa que ele é maior
+		//Que o minimo local encontrado até o momento.
 		d_lMax_S[tid] = lMin_R;		
 	}
 }
 
-void calcLMaxS(char* lMax_S, char* lMin_R, int tamVec){
+//Com os valores de máximos locais de S, calcular o máximo global.
+void calcLMaxGlobalS(char* lMax_globalS, char* lMax_localS, int tamVec){
 	//Número de conjuntos
 	for(int i = 0; i < tamVec; i++){
-		if(*lMax_S < lMin_R[i]){
-			*lMax_S = lMin_R[i];
+		if(*lMax_globalS < lMax_localS[i]){
+			*lMax_globalS = lMax_localS[i];
 		}
 	}
 }
+
 //Seja S o conjunto de todas las sequencias dos n primeiros números naturais.
 //Defina R(s), com s \in S o conjunto de todas as sequencias que podem
 //ser geradas rotacionando S.
@@ -152,42 +164,29 @@ int main(int argc, char *argv[]){
 	char* h_sequence;            //Vetor com a sequência pivor do grupo
 	char* h_threadSequences;      //Vetor com as sequências criadas
 	char* d_threadSequences;	    //Sequências produzidas para enviar para o device
-	char* d_lMax_S;      //Vetor com os resultados de cada thread. L Mínimos do conjunto de R
-	char* h_lMax_S;      
+	char* d_lMax_localS;      //Vetor com os máximos locais de S, cada thread tem um máximo local
+	char* h_lMax_localS;      
 
 	int length = N;
 	int NUM_THREADS = atoi(argv[1]);
 	
-
 	cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeEightByte);
-	cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
+	//cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
 	
-	//Tamanho linear da sequência que vai ser enviada para cada thread.
-	//Vetor consisti em Sua sequência seguida por repetição dos seus primeiros length-1 elementos devido a rotação.
-	//Ex: 1 2 3 4 1 2 3.
-	//int length = 2*length-1; 
-
-	//Tamanho linear de cada thread da Shared Memory, composto por:
-	//Vetor MR[N+1]*[N+1] com as sequência LIS e LDS mais promissoras
-	//Vetor Last[N] com o tamanho do ultimo valor de cada sequência promissora
-	//Sequência geradas de tamanho length
-	printf("Shared: %d\n", length*THREAD_PER_BLOCK);
 	clock_t start,end;
 
 	//Aloca memória dos vetores	
 	h_sequence = (char*) malloc(length);
 	h_threadSequences = (char*) malloc(length*NUM_THREADS);
-	h_lMax_S = (char*) malloc(NUM_THREADS);
+	h_lMax_localS = (char*) malloc(NUM_THREADS);
 	cudaMalloc(&d_threadSequences, length*NUM_THREADS);
-	cudaMalloc(&d_lMax_S, NUM_THREADS);
-	cudaMemset(d_lMax_S, 0, NUM_THREADS);
+	cudaMalloc(&d_lMax_localS, NUM_THREADS);
+	cudaMemset(d_lMax_localS, 0, NUM_THREADS);
 
 	//Gera a sequência primária, de menor ordem léxica	
 	for(int i = 0; i < length; i++)
 		h_sequence[i] = i+1;
-	unsigned int numSeqReady = 0; //Número de sequêcias prontas
-	
-	//char lMax_S = 0; //Resultado final, maior valor encontrado do grupo S
+	unsigned int numSeqReady = 0; //Número de sequêcias prontas para enviar para a GPU
 
 	start = clock();
 	
@@ -200,9 +199,9 @@ int main(int argc, char *argv[]){
 	//Cada loop gera um conjunto de sequências. Elementos de S. Cada elemento possui um conjunto de R sequencias.
 	while(counter){
 		//Gera todos os pivores do conjunto R
-		memcpy(h_threadSequences + numSeqReady*length,
-			   h_sequence, length);
+		memcpy(h_threadSequences + numSeqReady*length, h_sequence, length);
 		numSeqReady++;
+
 		//Caso não tenha como inserir mais un conjunto inteiro no número de threads, então executa:
 		if(numSeqReady == NUM_THREADS){
 			cudaMemcpy(d_threadSequences, h_threadSequences, numSeqReady*length, cudaMemcpyHostToDevice);
@@ -210,9 +209,10 @@ int main(int argc, char *argv[]){
 			dim3 num_blocks(ceil(((float) numSeqReady)/(float) THREAD_PER_BLOCK));
 			int tam_shared = length*THREAD_PER_BLOCK;
 
-			//Cada thread calcula: Min_{s' \in R(s)}(Min(|LIS(s)|, |LDS(s)|))
+			//Cada thread calcula: Min_{s' \in R(s)}(Min(|LIS(s)|, |LDS(s)|)), e se o resultado for maior que o máximo local,
+			//insere na variável
 			decideLS<<<num_blocks, THREAD_PER_BLOCK,  tam_shared>>>
-					   (d_threadSequences, d_lMax_S, length, numSeqReady);
+					   (d_threadSequences, d_lMax_localS, length, numSeqReady);
 			//Recomeça a gerar sequências
 			numSeqReady = 0; 
 		}	
@@ -234,24 +234,25 @@ int main(int argc, char *argv[]){
 		int tam_shared = ((length+1)*(length+1)+(3*length-1))*THREAD_PER_BLOCK*sizeof(int);
 		
 		decideLS<<<num_blocks,THREAD_PER_BLOCK, tam_shared>>>
-			       (d_threadSequences, d_lMax_S, length, numSeqReady);
+			       (d_threadSequences, d_lMax_localS, length, numSeqReady);
 		
 	}
 
-	cudaMemcpy(h_lMax_S, d_lMax_S, NUM_THREADS, cudaMemcpyDeviceToHost);
-	char lMax_S = 0;
-	calcLMaxS(&lMax_S, h_lMax_S, NUM_THREADS);	
+	cudaMemcpy(h_lMax_localS, d_lMax_localS, NUM_THREADS, cudaMemcpyDeviceToHost);
+
+	char lMax_globalS = 0; //Variável com o máximo global de S
+	calcLMaxGlobalS(&lMax_globalS, h_lMax_localS, NUM_THREADS);	
 
 	cudaThreadSynchronize();
 	end = clock();
 
 	printf("100%% - Tempo: %f s\n", (float)(end-start)/CLOCKS_PER_SEC);
 
-	printf("Lmax R = %d\n",lMax_S);
+	printf("Lmax R = %d\n",lMax_globalS);
 
 	free(h_sequence);
 	free(h_threadSequences);
-	free(h_lMax_S);
+	free(h_lMax_localS);
 	cudaFree(d_threadSequences);
-	cudaFree(d_lMax_S);
+	cudaFree(d_lMax_localS);
 }
