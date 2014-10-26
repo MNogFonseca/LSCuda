abe802517140bd9ab0303369b69495609b60e14c
matheus <matha.nogueira@gmail.com>
2014-10-26 12:16:32 -0200

    b

--- LSCuda/LDS.cu
+++ LSCuda/LDS.cu
@@ -1,9 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+__device__
+int MAP_MP(int n){
+	return n*(n-1)/2;
+}
+
+__device__
+void LDSprintMP(char* mat,int tam){
+	int i, j;
+	int pos = 0;
+	for(i = 1 ; i<tam+1 ; i++){
+		for(j = 0; j < i; j++, pos++){ 
+			printf("%d |",mat[pos]);
+		}
+		printf("\n");	
+		//if(mat[i]== -1){
+		//	break;
+		//}
+		//else
+		//	printf("%d -",mat[i]);
+
+	}
+	printf("\n");	
+}
+
+
 //pega o menor valor do vetor last que seja maior do que x
 __device__
-char LDSgetLast(char* last,int x,int tam){
+char LDSgetLast(char* last,char x,int tam){
 	int i;
 	for(i=0; i < tam ;i++){
 		if(last[i] < x ){
@@ -17,9 +42,9 @@ char LDSgetLast(char* last,int x,int tam){
 //pega a posicao valida para inserir um elemento no vetor vet
 __device__
 int LDSgetPos(char vet[],int tam){
-	for(int i =0;i < tam;i++){
-
-		if(vet[i]== -1){
+	int i;
+	for(i =0;i < tam;i++){
+		if(vet[i] == -1){
 			return i;
 		}
 	}
@@ -44,7 +69,7 @@ unsigned char LDS(char* vet, char* last, char* MP, int tam){
 	}
 	
 	char lmax = 1;  //maior tamanho de subsequencia
-	
+
 	for(int i = 1, pos = 0;i <= tam; i++){
 		for(int j = 0; j < i; j++, pos++){
 			MP[pos] = -1;
@@ -70,9 +95,10 @@ unsigned char LDS(char* vet, char* last, char* MP, int tam){
 
 			int pos = LDSgetPos(MP+MAP_MP(l),tam);			
 			MP[MAP_MP(l)+pos] = last[l-1];
+			//LDSprintMP(MP, tam);
 	}
 	if(lmax == 32){
-		LISprintMP(MP, tam);	
+		LDSprintMP(MP, tam);	
 	}
 	return lmax;
 }
