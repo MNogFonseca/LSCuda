056b79e2e0ce18168f4fb7fa1fcf5546deea1e9c
MNogFonseca <matha.nogueira@gmail.com>
2014-11-14 11:11:19 -0200

    Update nextperm.cu

--- LSCuda/nextperm.cu
+++ LSCuda/nextperm.cu
@@ -107,6 +107,15 @@ void decideLS(char *vector, char* d_lMax_S, int length, int maxSeq, int numThrea
 		}
 		indexSeq += numThreads;
 	}
+
+	__synchtrads();
+	if(tid == 0){
+		for(int k = 0; k < numThreads; k++){
+			for(int i = 0; i < 10; i++)
+				printf("%d -->\n",d_lMax_S[k]);
+			printf("\n");
+		}
+	}
 }
 
 //Com os valores de máximos locais de S, calcular o máximo global.
