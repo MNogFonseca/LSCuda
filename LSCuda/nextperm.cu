33555bb4a15c0daa4ee77d32beed17eba9681e4d
matheus <matha.nogueira@gmail.com>
2014-11-16 11:58:37 -0200

    b

--- LSCuda/nextperm.cu
+++ LSCuda/nextperm.cu
@@ -65,7 +65,7 @@ void decideLS(char* d_lMax_S, int length, unsigned long long maxSeq, int numThre
 	bool flagFinalLoop;
 	while(indexSeq < maxSeq){
 		getSequence(s_vet + s_index, length, indexSeq);
-		printVector(s_vet + s_index);
+		printVector(s_vet + s_index, length);
 		lMin_R = 20; //Variavel que representa o min encontrado no conjunto R
 		flagFinalLoop = true;
 		for(int i = 0; i < length; i++){ //Rotação
